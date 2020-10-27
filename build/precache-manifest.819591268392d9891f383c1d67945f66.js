e374a0c123a6253bcc5a36d5a5b6441af91b58bf
Bernal03 <61256617+Bernal03@users.noreply.github.com>
2020-10-26 19:42:17 -0700

    segundo deploy

---
+++ build/precache-manifest.819591268392d9891f383c1d67945f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70af08c124bc4768fefadeb371a8508f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab90cc8b370b45600479",
+    "url": "/static/css/main.8fd4bf60.chunk.css"
+  },
+  {
+    "revision": "38964d20703d486fc98c",
+    "url": "/static/js/2.405eebd3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.405eebd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab90cc8b370b45600479",
+    "url": "/static/js/main.a9b5bd1b.chunk.js"
+  },
+  {
+    "revision": "41f0d3531a017e65c599",
+    "url": "/static/js/runtime-main.ff3d99b6.js"
+  },
+  {
+    "revision": "96aff77c5833d91511a87f051dc63231",
+    "url": "/static/media/logoTec.96aff77c.jpg"
+  }
+]);
